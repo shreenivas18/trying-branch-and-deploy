a7c223f99a985fb876238752cdb85f2eb8ee9f79
shreenivas18 <shreenivasnaik280@gmail.com>
2025-06-20 22:39:18 +0530

    on branch to vercel

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> this is deployed on vercel from branch
+          Edit <code>src/App.js</code> — this is deployed on Vercel from branch
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn how to craete branch and push
+          Learn how to create a branch and push
         </a>
       </header>
     </div>
